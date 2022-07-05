86aa6f5018112f7e16fcd695930451b4747bdb17
Giuliano <Darkjason55@gmail.com>
2022-07-05 13:22:33 +0300

    Render a lot of cubes with now texture class

--- FireLight/main.cpp
+++ FireLight/main.cpp
@@ -1,20 +1,30 @@
 #include "FireLight.h"
 
+#include <vector>
+
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#define STB_IMAGE_IMPLEMENTATION
-#include "thirdParty/stb_image.h"
+#include "src/OpenGL/Texture.h"
 
 unsigned int VBO, VAO;
 
+struct GameObject
+{
+    unsigned int VBO, VAO;
+    glm::mat4 model = glm::mat4(1.0f);
+};
+
 int main()
 {
+
+    std::vector<GameObject> objects;
+
     glm::mat4 view = glm::mat4(1.0f);
     glm::mat4 cameraMatrix = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
     glm::mat4 transform = glm::mat4(1.0f);
-    view = glm::translate(view, glm::vec3(0, 0, -3.0f));
+    view = glm::translate(view, glm::vec3(0, 0, -10.0f));
     transform = glm::rotate(transform, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 
     float vertices[] = {
@@ -60,70 +70,56 @@ int main()
         -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
         -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
+
     InitWindow(800, 600, "FireLight Alpha");
 
-    //Loading the texture
-    int widht, height, nrChannels;
-    unsigned char* data = stbi_load("Resources/container.jpg", &widht, &height, &nrChannels, 0);
-
-    //Generate Texture
-    unsigned int texture;
-    glGenTextures(1, &texture);
-
-    glBindTexture(GL_TEXTURE_2D, texture);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    // set texture filtering parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, widht, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-    glGenerateMipmap(GL_TEXTURE_2D);
-
-    stbi_image_free(data);
-
+    Texture texture = Texture::LoadTextureFromFile("Resources/container.jpg");
     Shader shader = Shader::LoadFromFile("Resources/first.vert", "Resources/first.frag");
 
-    unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-    glBindVertexArray(VAO);
+    for (int i = 0; i < 9; i++)
+    {
+        GameObject obj;
+        obj.model = glm::translate(obj.model, glm::vec3(i % 3 * 2.0,0, floor(i / 3) * 2.0));
+        glGenVertexArrays(1, &obj.VAO);
+        glGenBuffers(1, &obj.VBO);
+        // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+        glBindVertexArray(obj.VAO);
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    // texture coord attribute
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, obj.VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+        // position attribute
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+        // texture coord attribute
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(1);
 
-    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+        // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-    glBindVertexArray(0);
+        // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+        // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+        glBindVertexArray(0);
+
+        objects.push_back(obj);
+    }
 
     glEnable(GL_DEPTH_TEST);
 	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
-
 	while (SetUpFrame())
 	{
-        transform = glm::mat4(1.0f);
-        transform = glm::rotate(transform, glm::radians((float)glfwGetTime() * 100.0f), glm::vec3(1.0f, 1.0f, 0.0f));
+        for (int i = 0; i < 9; i++)
+        {
+            texture.Bind();
+            shader.Use();
+            shader.SetMat4("projection", cameraMatrix);
+            shader.SetMat4("view", view);
+            shader.SetMat4("model", objects[i].model);
 
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, texture);
-        shader.Use();
-        shader.SetMat4("projection", cameraMatrix); 
-        shader.SetMat4("view", view);
-        shader.SetMat4("model", transform);
-		glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+            glBindVertexArray(objects[i].VAO);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
 		UpdateWindow();
 	}
--- FireLight/src/OpenGL/Texture.h
+++ FireLight/src/OpenGL/Texture.h
@@ -1,1 +1,43 @@
 #pragma once
+
+#include <gl/glew.h>
+
+#include <string>
+#define STB_IMAGE_IMPLEMENTATION
+#include "../../thirdParty/stb_image.h"
+
+
+struct Texture
+{
+	static Texture LoadTextureFromFile(const std::string& path)
+	{
+		Texture temp;
+		int widht, height, nrChannels;
+		unsigned char* data = stbi_load(path.c_str(), &widht, &height, &nrChannels, 0);
+
+		glGenTextures(1, &temp.ID);
+
+		glBindTexture(GL_TEXTURE_2D, temp.ID);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		// set texture filtering parameters
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, widht, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		stbi_image_free(data);
+
+		return temp;
+	}
+
+	void Bind()
+	{
+		glBindTexture(GL_TEXTURE_2D, ID);
+	}
+
+
+	unsigned int ID;
+};
