ff0fb32866ae55624e99ac6830d711f12e386dd3
Giuliano <Darkjason55@gmail.com>
2022-11-06 13:13:20 +0200

    FullCleanUp

--- README.md
+++
@@ -1,3 +0,0 @@
-# FireLight
-
-GameEngine
--- build.bat
+++ build.bat
@@ -1,4 +1,15 @@
-msbuild FireLight.sln
-cd libBUILD
-START FireLight.exe
-cd ..
+@echo off
+cls
+
+SET includes=/external:W0 /MD /external:I "F:\vcpkg\vcpkg\installed\x64-windows\include"
+SET libs=/link /LIBPATH:"F:\vcpkg\vcpkg\installed\x64-windows\lib\" glfw3dll.lib glad.lib opengl32.lib
+SET initial=/nologo /std:c++20 /W0 /EHsc
+
+
+if not exist build mkdir build
+
+pushd build 
+ccache cl %initial% %includes% ..\main.cpp %libs%
+popd
+
+.\build\main.exe
